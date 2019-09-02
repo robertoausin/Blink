d2a18418f6b682db5582409b5e4a4998f4adea2f
robertoausin <robertoausin@gmail.com>
2019-09-02 20:05:06 +0200

    Segundo Commit

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,18 +21,17 @@
 
   http://www.arduino.cc/en/Tutorial/Blink
 */
-#define PIN 2
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(PIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
