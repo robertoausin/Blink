da1db5cde65010b7a0adf91aa15012a715d36bb6
robertoausin <robertoausin@gmail.com>
2019-09-02 20:17:07 +0200

    Encendido y apagado 1 LED 500 ms

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -10,7 +10,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(ledRojo, HIGH);      // turn the red LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(500);                      // wait for a second
   digitalWrite(ledRojo, LOW);       // turn the red LED off (LOW is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(500);                      // wait for a second
 }
