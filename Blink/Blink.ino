c8a714d070011fc61e40dedcb68283a4497b2c92
robertoausin <robertoausin@gmail.com>
2019-09-02 20:15:37 +0200

    Encendido y apagado 1 LED

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,20 +1,16 @@
-// Parpadeo 2 LED //
+// Parpadeo 1 LED //
 
 #define ledRojo 2
-#define ledAzul 3
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ledRojo, OUTPUT);
-  pinMode(ledAzul, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(ledRojo, HIGH);      // turn the red LED on (HIGH is the voltage level)
-  digitalWrite(ledAzul, LOW);       // turn the blue LED off (LOW is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(ledRojo, LOW);       // turn the red LED off (LOW is the voltage level)
-  digitalWrite(ledAzul, HIGH);      // turn the blue LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
 }
